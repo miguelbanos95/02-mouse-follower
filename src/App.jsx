1d92bd665b1a269d6336e6f6a3046da2c891bcec
Miguel Baños <info.miguelbanos@gmail.com>
2023-02-27 20:00:22 +0100

    create mouse project + useEffects

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    // console.log('efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      // clientX y clientY son constantes reservadas para determinar la posición del puntero
+      // console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // Sirve para limpiar el evento
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default App
