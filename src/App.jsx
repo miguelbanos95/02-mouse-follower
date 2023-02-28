c683a721a8258ca92a1f26c04cad91c5f3f5ad54
Miguel Baños <info.miguelbanos@gmail.com>
2023-02-28 22:53:01 +0100

    add sizes buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App () {
   const [position, setPosition] = useState({ x: -25, y: -25 })
   const [color, setColor] = useState('0, 0, 0, 0.5')
   const [isHovering, setIsHovering] = useState(false)
+  const [decrement, setDecrement] = useState({ h: 50, w: 50 })
+  const [isLeft, setIsLeft] = useState({ l: -25 })
+  const [isRight, setIsRight] = useState({ r: -25 })
 
   useEffect(() => {
     // console.log('efecto', { enabled })
@@ -50,9 +53,37 @@ function App () {
   const handleMouseOut = () => {
     setIsHovering(false)
   }
+  const handleReset = () => {
+    setColor({ r: 0, g: 0, b: 0.5 })
+    setPosition({ x: -25, y: -25 })
+    setEnabled(false)
+    setDecrement({ h: 50, w: 50 })
+    setIsLeft({ l: -25 })
+    setIsRight({ r: -25 })
+  }
+  const handleDecrement = () => {
+    const lessH = decrement.h - 10
+    const lessW = decrement.w - 10
+    setDecrement({ h: lessH, w: lessW })
+
+    const addLeft = isLeft.l + 5
+    const addRight = isRight.r + 5
+    setIsLeft({ l: addLeft })
+    setIsRight({ r: addRight })
+  }
+  const handleIncrement = () => {
+    const moreH = decrement.h + 10
+    const moreW = decrement.w + 10
+    setDecrement({ h: moreH, w: moreW })
+
+    const restLeft = isLeft.l - 5
+    const restRight = isRight.r - 5
+    setIsLeft({ l: restLeft })
+    setIsRight({ r: restRight })
+  }
 
   return (
-    <main style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <main style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
       <div
         style={{
           position: 'absolute',
@@ -61,26 +92,36 @@ function App () {
           borderRadius: '50%',
           opacity: 0.8,
           pointerEvents: 'none',
-          left: -25,
-          top: -25,
-          width: 50,
-          height: 50,
+          left: isLeft.l,
+          top: isRight.r,
+          width: decrement.w,
+          height: decrement.h,
           transform: `translate(${position.x}px, ${position.y}px)`
         }}
       />
-      <div className='bt-section'>
-        <button>
-          Cambia de tamaño
-        </button>
-        <button onClick={handleClick} className='btn bt-activar'>
-          {enabled ? 'Desactivar' : 'Activar'}  Seguir Puntero
-        </button>
-
+      <div className='bt-section1'>
         <button
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
         >
           Cambia de color
+        </button>
+      </div>
+
+      <div className='bt-section2'>
+        <button onClick={handleIncrement}>
+          + Tamaño
+        </button>
+        <button onClick={handleClick} className='btn bt-activar'>
+          {enabled ? 'Desactivar' : 'Activar'}  Seguir Puntero
+        </button>
+        <button onClick={handleDecrement}>
+          - Tamaño
+        </button>
+      </div>
+      <div className='bt-section3'>
+        <button onClick={handleReset}>
+          Reset
         </button>
       </div>
     </main>
